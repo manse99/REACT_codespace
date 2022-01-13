f4b9b2e61ed8314b62ceedc4e73ba9fb3c646f96
“mansee99” <manse99@gmail.com>
2022-01-13 13:19:43 -0500

    REACT start page

---
+++ MyReact.jsx
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="index.css">
+  <script crossorigin="" src="https://unpkg.com/react@17/umd/react.development.js"></script>
+  <script crossorigin="" src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
+  <title>My React</title>
+</head>
+<body>
+  <h1 hello="" gabtiel=""></h1>
+  <script type="text/javascript" src=""></script>
+</body>
+</html>
